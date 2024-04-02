29458ef312df04751974412b6cfb51f3868c9691
Igor <80968651+webdevbiv@users.noreply.github.com>
2024-04-02 17:12:08 +0300

    UPD | NewPost

--- section-02/react-crash-course/src/components/NewPost/NewPost.jsx
+++ section-02/react-crash-course/src/components/NewPost/NewPost.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
+import styles from "./NewPost.module.css";
+
 const NewPost = () => {
+  const [body, setBody] = useState("");
+  function changeBodyHandler(e) {
+    setBody(e.target.value);
+  }
+
   return (
-    <form>
+    <form className={styles.form}>
       <p>
-        <label htmlFor="body"></label>
-        <textarea id="body" required rows={3}></textarea>
+        <label htmlFor="body">Text</label>
+        <textarea
+          id="body"
+          required
+          rows={3}
+          onChange={changeBodyHandler}
+        ></textarea>
       </p>
+      <p>{body}</p>
       <p>
-        <label htmlFor="name"></label>
+        <label htmlFor="name">Name</label>
         <textarea name="" id="name" required></textarea>
       </p>
     </form>
--- section-02/react-crash-course/src/components/PostsList/PostsList.jsx
+++ section-02/react-crash-course/src/components/PostsList/PostsList.jsx
@@ -1,13 +1,17 @@
+import NewPost from "../NewPost/NewPost";
 import Post from "../Post/Post";
 import styles from "./PostsList.module.css";
 
 function PostsList() {
   return (
-    <ul className={styles.posts}>
-      <Post author="John" body="React is awesome" />
+    <>
+      <NewPost />
+      <ul className={styles.posts}>
+        <Post author="John" body="React is awesome" />
 
-      <Post author="Jane" body="Next.js is awesome" />
-    </ul>
+        <Post author="Jane" body="Next.js is awesome" />
+      </ul>
+    </>
   );
 }
 
