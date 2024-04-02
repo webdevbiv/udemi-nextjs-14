5057d334711aa9b24a7a2e266d612ba3be59645f
Igor <80968651+webdevbiv@users.noreply.github.com>
2024-04-02 05:59:11 +0300

    UPD | Post

--- section-02/react-crash-course/src/components/Post.jsx
+++ section-02/react-crash-course/src/components/Post.jsx
@@ -1,8 +1,13 @@
 const names = ["Dima", "Alex", "Kate"];
 
 function Post() {
-  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
-  return <div>Post</div>;
+  // Generate a random index based on the length of the names array
+  const randomIndex = Math.floor(Math.random() * names.length);
+  // Use the random index to select a name from the names array
+  const chosenName = names[randomIndex];
+
+  // Return a div that includes the chosen name
+  return <div>{`Post by ${chosenName}`}</div>;
 }
 
 export default Post;
