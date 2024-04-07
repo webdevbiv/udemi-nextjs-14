5b7ca533db8ace1a76e2667cf2e996bd984aea3a
Igor <80968651+webdevbiv@users.noreply.github.com>
2024-04-07 11:08:35 +0300

    ADD | meal-grid, meal-item components

--- section-03/foodies-project/app/meals/page.jsx
+++ section-03/foodies-project/app/meals/page.jsx
@@ -1,12 +1,26 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 
 function MealsPage() {
   return (
-    <main>
-      <h1>Meals Page</h1>
-      <Link href="/meals/1">Meal 1</Link>
-      <Link href="/meals/2">Meal 2</Link>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
 
---
+++ section-03/foodies-project/components/meals/meals-grid.jsx
@@ -0,0 +1,15 @@
+import { Linter } from "eslint";
+import React from "react";
+import classes from "./meals-grid.module.css";
+
+export default function MealsGrid({ meals }) {
+  return (
+    <ul className={classes.meals}>
+      {meals.map((meal) => (
+        <li key={meal.id}>
+          <MealItem {...meal} />
+        </li>
+      ))}
+    </ul>
+  );
+}
