050bdec854cdf17c0799e80b50a33ce2e659aabe
Igor <80968651+webdevbiv@users.noreply.github.com>
2024-04-07 05:14:02 +0300

    UPD | navigation

--- section-03/foodies-project/components/main-header/main-header.jsx
+++ section-03/foodies-project/components/main-header/main-header.jsx
@@ -1,10 +1,16 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import MainBackground from "./main-header-background";
 
 function MainHeader() {
+  const pathname = usePathname();
+  console.log(pathname);
+
   return (
     <>
       <MainBackground />
@@ -17,10 +23,20 @@ function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <Link
+                href="/meals"
+                className={pathname === "/meals" ? classes.active : ""}
+              >
+                Browse Meals
+              </Link>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <Link
+                href="/community"
+                className={pathname === "/community" ? classes.active : ""}
+              >
+                Foodies Community
+              </Link>
             </li>
           </ul>
         </nav>
