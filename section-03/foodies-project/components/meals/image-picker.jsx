81f3f7b5a1431ca2d20e8fdc11e20de4fdfaf115
Igor <80968651+webdevbiv@users.noreply.github.com>
2024-04-08 15:07:30 +0300

    ADD | not-found

---
+++ section-03/foodies-project/components/meals/image-picker.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import classes from "./image-picker.module.css";
+export default function ImagePicker({ label, name }) {
+  const inputRef = useRef();
+  function handlePickClick() {
+    inputRef.current.click();
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={inputRef}
+        />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickClick}
+        >
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+}
