3b1cb46cfd533e497bfc0744396c9f620e4c5891
Igor <80968651+webdevbiv@users.noreply.github.com>
2024-04-08 18:13:12 +0300

    UPD | image-picker

--- section-03/foodies-project/components/meals/image-picker.jsx
+++ section-03/foodies-project/components/meals/image-picker.jsx
@@ -1,17 +1,40 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import Image from "next/image";
 import classes from "./image-picker.module.css";
 export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
   const inputRef = useRef();
   function handlePickClick() {
     inputRef.current.click();
+  }
+
+  function handleImageChange(e) {
+    const file = e.target.files[0];
+    if (!file) {
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
   }
 
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage ? (
+            <p>No image picked yet.</p>
+          ) : (
+            <Image src={pickedImage} alt="Picked Image" fill />
+          )}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -19,6 +42,7 @@ export default function ImagePicker({ label, name }) {
           accept="image/png, image/jpeg"
           name={name}
           ref={inputRef}
+          onChange={handleImageChange}
         />
         <button
           className={classes.button}
