b994f727c23df646f9eedd40465d279075bd5c53
Igor <80968651+webdevbiv@users.noreply.github.com>
2024-04-09 11:54:54 +0300

    ADD | server data error

--- section-03/foodies-project/app/meals/share/page.jsx
+++ section-03/foodies-project/app/meals/share/page.jsx
@@ -1,10 +1,15 @@
+"use client";
+
+import { useFormState } from "react-dom";
+
 import ImagePicker from "@/components/meals/image-picker";
 import MealFormButton from "@/components/meals/meal-form-button";
-
-import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
 
+import classes from "./page.module.css";
+
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
   return (
     <>
       <header className={classes.header}>
@@ -14,7 +19,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -43,6 +48,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealFormButton />
           </p>
--- section-03/foodies-project/lib/actions.js
+++ section-03/foodies-project/lib/actions.js
@@ -3,8 +3,12 @@
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 
-export async function shareMeal(formData) {
+function isInvalidText(text) {
+  return !text || text.trim() === "";
+}
+export async function shareMeal(prevState, formData) {
   "use server";
+
   const meal = {
     title: formData.get("title"),
     summary: formData.get("summary"),
@@ -13,6 +17,20 @@ export async function shareMeal(formData) {
     creator: formData.get("name"),
     creator_email: formData.get("email"),
   };
+
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    return { message: "Invalid input." };
+  }
+
   await saveMeal(meal);
   redirect("/meals");
 }
