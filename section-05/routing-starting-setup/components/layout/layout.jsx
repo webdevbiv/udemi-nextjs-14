163a2018a6e10814c5548fb963ea18c0cbbab9fa
Igor <80968651+webdevbiv@users.noreply.github.com>
2024-04-10 18:16:17 +0300

    ADD | layout & main-header components

---
+++ section-05/routing-starting-setup/components/layout/layout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MainHeader from "./main-header";
+
+export default function Layout(props) {
+  return (
+    <>
+      <MainHeader />
+      <main>{props.children}</main>
+    </>
+  );
+}
